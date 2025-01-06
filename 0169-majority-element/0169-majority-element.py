d33eb2f857d22dc0552c1c046ee14a04a2218a4f
Mori <hwbae42@gmail.com>
2025-01-06 11:56:50 +0900

    Time: 4 ms (71.1%), Space: 19.7 MB (6.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,9 +2,7 @@ from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        count_dict = Counter(nums)
-        for num in count_dict:
-            if count_dict[num] > n // 2:
+        threshold = len(nums) // 2
+        for num, count in Counter(nums).items():
+            if count > threshold:
                 return num
-        return None
